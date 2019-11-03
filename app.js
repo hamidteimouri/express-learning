f26c993c461cd18c5724aa355905ee78f2607427
Hamid Teimouri <hamidteimouri@outlook.com>
2019-11-03 13:34:29 +0330

    404 route

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
     res.send('Hello world');
 });
 // define a route
-app.get('/profile', function (req, res) {
+app.get('/first', function (req, res) {
     res.send('some text here...');
 });
 
@@ -16,14 +16,20 @@ app.get('/profile/:name', function (req, res) {
     res.send("<h1>" + nameVar + "</h1>");
 });
 // define a route with optional parameter
-app.get('/profile2/:name?', function (req, res) {
+app.get('/panel/:name?', function (req, res) {
     // define a variable
     if (req.params.name)
         var nameVar = req.params.name;
     else
-        nameVar = 'No Parameter';
+        nameVar = 'No Parameter set';
 
-    res.send("<h1> this is a optional : " + nameVar + "</h1>");
+    //res.send("<h1> this is a optional : " + nameVar + "</h1>");
+    res.send(`<h1> this is a optional :  ${nameVar} </h1>`);
+});
+
+// implement 404 page
+app.get('*', function (req, res) {
+    res.send("<h1>404 page</h1>");
 });
 
 var server = app.listen(3000, function () {

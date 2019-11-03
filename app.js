69d885346520b46c2c354d44676890a5b624f77b
Hamid Teimouri <hamidteimouri@outlook.com>
2019-11-03 12:08:26 +0330

    routing and parameter

--- app.js
+++ app.js
@@ -13,13 +13,17 @@ app.get('/profile', function (req, res) {
 app.get('/profile/:name', function (req, res) {
     // define a variable
     var nameVar = req.params.name;
-    res.send(nameVar);
+    res.send("<h1>" + nameVar + "</h1>");
 });
 // define a route with optional parameter
 app.get('/profile2/:name?', function (req, res) {
     // define a variable
-    var nameVar = req.params.name;
-    res.send(nameVar);
+    if (req.params.name)
+        var nameVar = req.params.name;
+    else
+        nameVar = 'No Parameter';
+
+    res.send("<h1> this is a optional : " + nameVar + "</h1>");
 });
 
 var server = app.listen(3000, function () {

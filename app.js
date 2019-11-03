4c131c04c041604ac62d75546f90820e976cb9c1
Hamid Teimouri <hamidteimouri@outlook.com>
2019-11-03 11:49:52 +0330

    routing and parameters

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello world');
 });
+// define a route
 app.get('/profile', function (req, res) {
     res.send('some text here...');
+});
+
+// define a route with parameter
+app.get('/profile/:name', function (req, res) {
+    // define a variable
+    var nameVar = req.params.name;
+    res.send(nameVar);
 });
 
 var server = app.listen(3000, function () {
